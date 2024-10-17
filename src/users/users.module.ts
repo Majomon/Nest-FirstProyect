2059fcb382811c3105fe618259f19a62cd2d3ce1
Mauri <mauri.monzon91@gmail.com>
2024-10-16 21:27:38 -0300

    Proveedores

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,13 +4,31 @@ import { UsersRepository } from './users.repository';
 import { UsersService } from './users.service';
 import { LoggerMiddleware } from 'src/middlewares/logger.middleware';
 
-const mockUserService = {
+/* const mockUserService = {
   getUsers: () => 'Esto es un servicio MOCK de usuarios',
-};
+}; */
 
 @Module({
-  // providers: [UsersService, UsersRepository],
-  providers: [{ provide: UsersService, useValue: mockUserService }],
+  providers: [
+    UsersService,
+    UsersRepository,
+    {
+      provide: 'API_USERS',
+      useFactory: async () => {
+        const apiUsers = await fetch(
+          'https://jsonplaceholder.typicode.com/users',
+        ).then((response) => response.json());
+        return apiUsers.map((user) => {
+          return {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+          };
+        });
+      },
+    },
+  ],
+  // providers: [{ provide: UsersService, useValue: mockUserService }],
   controllers: [UsersController],
 })
 export class UsersModule implements NestModule {
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,10 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { UsersRepository } from './users.repository';
 
 @Injectable()
 export class UsersService {
-  constructor(private usersRepository: UsersRepository, ) {}
-  getUsers() {
-    return this.usersRepository.getTodo();
+  constructor(
+    private usersRepository: UsersRepository,
+    @Inject('API_USERS') private apiUsers: any,
+  ) {}
+  async getUsers() {
+    const dbUSers = await this.usersRepository.getTodo();
+    const users = [...dbUSers, ...this.apiUsers];
+    return users;
   }
 }
