9f71bb3beef4d2dd68844bf1f455137aa0082450
Mauri <mauri.monzon91@gmail.com>
2024-10-17 10:46:40 -0300

    Configurado typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,24 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { TodosModule } from './todos/todos.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 // import { APP_INTERCEPTOR } from '@nestjs/core';
 // import { APP_GUARD } from '@nestjs/core';
 // import { AuthGuard } from './guards/auth.guard';
 
 @Module({
-  imports: [UsersModule, TodosModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      database: 'demo-typeorm',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'admin',
+    }),
+    UsersModule,
+    TodosModule,
+  ],
   controllers: [],
   // Manera 2 para hacer el Guard de forma global
   providers: [
