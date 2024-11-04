523e12d16d6b072ad1bbe9c9731a4ae67bda0bd2
Mauri <mauri.monzon91@gmail.com>
2024-11-03 22:54:38 -0300

    Update

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -6,25 +6,37 @@ dotenvConfig({
   path: './.env.development',
 });
 
+// const config = {
+//   type: 'postgres',
+//   database: process.env.POSTGRES_DATABASE,
+//   host: process.env.POSTGRES_HOST,
+//   port: process.env.DB_PORT as unknown as number,
+//   username: process.env.POSTGRES_USER,
+//   password: process.env.POSTGRES_PASSWORD,
+//   autoLoadEntities: true,
+//   // Para desarrollo
+//   // synchronize: true,
+//   // Para produccion se debe hacer la migración manual
+//   synchronize: false,
+//   logging: true,
+//   entities: ['dist/**/*.entity{.ts,.js}'],
+//   migrations:["dist/migrations/*{.ts,.js}"],
+// /*   ssl: {
+//     rejectUnauthorized: false,  // Importante para evitar problemas con certificados autofirmados
+//   }, */
+// };
+
 const config = {
   type: 'postgres',
-  database: process.env.DB_NAME,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT as unknown as number,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
+  url: process.env.POSTGRES_URL, // Usar la URL de conexión completa
   autoLoadEntities: true,
-  // Para desarrollo
-  // synchronize: true,
-  // Para produccion se debe hacer la migración manual
-  synchronize: false,
+  synchronize: false, // Ajusta a true solo en desarrollo; en producción usa migraciones
   logging: true,
   entities: ['dist/**/*.entity{.ts,.js}'],
-  migrations:["dist/migrations/*{.ts,.js}"],
-/*   ssl: {
-    rejectUnauthorized: false,  // Importante para evitar problemas con certificados autofirmados
-  }, */
+  migrations: ['dist/migrations/*{.ts,.js}'],
 };
+
+
 export default registerAs('typeorm', () => config);
 
 export const connectionSource = new DataSource(config as DataSourceOptions);
--- src/migrations/1730685203116-new.ts
+++ src/migrations/1730685203116-new.ts
@@ -1,12 +1,12 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class New1730303096728 implements MigrationInterface {
-    name = 'New1730303096728'
+export class New1730685203116 implements MigrationInterface {
+    name = 'New1730685203116'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "files" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, "mimeType" character varying NOT NULL, "data" bytea NOT NULL, "todo_id" integer, CONSTRAINT "PK_6c16b9093a142e0e7613b04a3d9" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TABLE "todos" ("id" SERIAL NOT NULL, "title" character varying NOT NULL, "description" character varying NOT NULL, "isCompleted" boolean NOT NULL DEFAULT false, CONSTRAINT "PK_ca8cafd59ca6faaf67995344225" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`CREATE TABLE "users" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "email" character varying NOT NULL, "password" character varying NOT NULL, "createdAt" character varying NOT NULL, "isAdmin" boolean NOT NULL DEFAULT true, CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "users" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "email" character varying NOT NULL, "password" character varying NOT NULL, "createdAt" character varying NOT NULL, "isAdmin" boolean NOT NULL DEFAULT false, CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`);
         await queryRunner.query(`ALTER TABLE "files" ADD CONSTRAINT "FK_925a2ba6622805fbc6acf2a1578" FOREIGN KEY ("todo_id") REFERENCES "todos"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
     }
 
